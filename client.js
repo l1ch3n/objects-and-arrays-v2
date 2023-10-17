a94b018e766450011047cf11d719170be08848ac
Nicole <nicole.shukin@gmail.com>
2023-10-16 22:16:39 -0600

    updates

---
+++ client.js
@@ -0,0 +1,38 @@
+// arrays
+let names = ['Frodo', 'Sam', 'Merry', 'Pippin', 'Gandalf', 'Aragon', 'Legolas', 'Gimli', 'Boromir'];
+
+names.push('Bilbo');
+
+let numbers = [1, 2, 3, 4, 5];
+
+numbers.push(6);
+
+// mixed array
+
+let mixed = ['Ash', 10, 'Tony', 20, 'Terry', 30, 40];
+
+mixed.push('Dasha');
+mixed.push(40);
+mixed[4] = 50;
+
+// objects
+
+let ninja = {
+  name: 'Naruto',
+  nation: 'Hidden Leaf',
+  age: 15
+};
+
+let ninja = {
+  name: 'Gaara',
+  nation: 'Hidden Sand',
+  age: 15
+};
+
+let ninja = {
+  name: 'Sasuke',
+  nation: 'Hidden Leaf',
+  age: 15
+};
+
+ninja.age = 30;
